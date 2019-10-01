68b611308f0ded33a3234847ce82baa9ae30e0ed
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-10-01 13:53:41 +0200

    add a store

---
+++ src/components/ChannelList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChannelList extends Component {
+    render() {
+        return (
+            <div>
+                Render ChannelList here!                 
+            </div>
+        )
+    }
+}
